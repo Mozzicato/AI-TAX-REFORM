a73ca2cec2a27950d698e29fb3f89bca495894d4
mubarak <mozzicato@email.com>
2026-01-20 23:20:17 +0100

    fuck graph rag

--- backend/app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Routes package for NTRIA API
-"""
-
-from . import chat_routes
-
-__all__ = ["chat_routes"]
--- deploy_to_railway.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# DEPRECATED: Railway deployment script
-# This file previously contained a script to deploy to Railway.
-# The repository has been migrated to Render. See RENDER_DEPLOYMENT_INSTRUCTIONS.txt
-# for the updated deployment guide and environment variable setup.
-
-echo "This script has been deprecated. See RENDER_DEPLOYMENT_INSTRUCTIONS.txt"
-exit 0
